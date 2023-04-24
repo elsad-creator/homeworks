4619cb6b364ffe465ea2c0f7cb03ae722ddfc7a0
elsad-creator <elshadd909@gmail.com>
2023-04-25 01:06:11 +0400

    task2

---
+++ task2/script.js
@@ -0,0 +1,19 @@
+// function tersCevir(cumle){
+//     return cumle.split(" ").reverse().join(" ")
+// }
+// let cumle = "salam ey dunya"
+
+// console.log(tersCevir(cumle));
+
+//! task2
+// let herf = "A"
+// let herf2 = "a"
+// console.log(herf.charCodeAt());
+// console.log(herf2.charCodeAt());
+
+//! task3
+// function toString(soz){
+//     return soz.split("").reverse().join("")
+// }
+// let soz = "hettuba"
+// console.log(toString(soz));
